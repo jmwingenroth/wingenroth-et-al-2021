8f2d712d348bf49ab15035349ab99500eb605ef2
jwing421 <jwing421@gmail.com>
2020-08-20 14:23:52 -0700

    changed column names to make table join work

--- code/etaplot.r
+++ code/etaplot.r
@@ -1,3 +1,21 @@
+## READ DATA
+
 library(tidyverse)
 
-list.files()
+files <- list.files("../data/k_runs/")
+pump <- str_detect(files, "pump")
+trap <- str_detect(files, "trap")
+
+pumpdata <- lapply(paste0("../data/k_runs/",files[pump]), read_csv)
+trapdata <- lapply(paste0("../data/k_runs/",files[trap]), read_csv)
+
+names(pumpdata) <- str_sub(files[pump],,6)
+names(trapdata) <- str_sub(files[trap],,6)
+
+metadata <- read_csv("../data/flumeexperimentsettings.csv")
+
+## K_TOT MODELS
+
+colnames(pumpdata) <- lapply(pumpdata, function(x) colnames(x) <- tolower(colnames(x)))
+
+lapply(pumpdata, function(x) rename(.data = x, t = "Time (s)", mvc = "mass volume concentration (ppm)"))
