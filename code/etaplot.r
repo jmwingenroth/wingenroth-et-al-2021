37518c859915b82d1e720570d93131b23345c274
jwing421 <jwing421@gmail.com>
2020-08-20 14:55:38 -0700

    everything runs smoothly. next up is k_c and eta calcs.

--- code/etaplot.r
+++ code/etaplot.r
@@ -12,6 +12,8 @@ trapdata <- lapply(paste0("../data/k_runs/",files[trap]), read_csv)
 
 metadata <- read_csv("../data/flumeexperimentsettings.csv")
 
+
+
 ## TIDY DATA
 
 names(pumpdata) <- str_sub(files[pump],,6)
@@ -19,4 +21,33 @@ names(trapdata) <- str_sub(files[trap],,6)
 
 pumpdata <- bind_rows(pumpdata, .id = "run")
 
-pumpdata
+pumpdata <- pumpdata %>%
+  rename(mvc = `mass volume concentration (ppm)`, t = `Time (s)`)
+
+
+
+## K_TOT MODELS
+
+k_fits <- lmList(log(mvc) ~ t | run, data = pumpdata)
+
+lm_results <- as.tibble(summary(k_fits)$coefficients[,,'t']) %>%
+  bind_cols(id = names(k_fits)) %>%
+  left_join(metadata, by = c("id" = "Date")) %>%
+  select(id, 
+         k_t = Estimate, 
+         dk_t = `Std. Error`, 
+         density = Density, 
+         freq = Velocity, 
+         biofilm = Biofouled)
+
+
+
+## K_C AND ETA CALCULATIONS
+
+df <- tibble(id = names(trapdata),
+       m_s = unlist(lapply(trapdata, function(x) mean(as.numeric(x$Sediment),na.rm = T)))) %>%
+  left_join(lm_results, by = "id")
+
+
+
+## PLOTS
